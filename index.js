f382e48c270c95472401ab1f4de57409360cb821
Kush <kushpatel124@gmail.com>
2022-12-03 22:30:59 -0500

    commit tree ac90f720ac7b6b5102d5125b623b3b2e137b7a57

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const feet = 2000;
+
+function scuberGreetingForFeet(feet) {
+  let cost;
+  if (feet <= 400) {
+    cost = 'This one is on me!';
+  } else if (feet > 400, feet <= 2000) {
+    cost = 'That will be twenty bucks.';
+  } else if (feet > 2000, feet <= 2500) {
+    cost = 'I will gladly take your thirty bucks.';
+  } else {
+    cost = 'No can do.';
+  }
+  return cost;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'Generous Tip':
+      console.log('Thank you so much.');
+      break;
+    case 'Not Generous Tip':
+      console.log('Thank you.');
+      break;
+    default:
+      console.log('Bye.');
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const city = NYC;
+function ternaryCheckCity(city) {
+  return (city ? ('Ok, sounds good.') : ('No go.'))
 }
